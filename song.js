e6b32c55eea64d5729449c08f9ee64303e5e0239
Jesse Gibson <jesse_gibson@me.com>
2015-06-11 07:48:11 +0000

    Changed the history array from an instance variable to a prototype property of Song, and also cleaned up some whitespace.

--- song.js
+++ song.js
@@ -1,22 +1,23 @@
-var song = {
-    repeat: false,
-    playlist: [],
-    history: [],
-}
-    
+var Song = function() {
+    this.repeat = false;
+    this.playlist = [];
+};
+
+Song.prototype.history = [];
+
 Song.prototype.add = function(playlist) {
     if (playlist.length === 0) return;
     
     for (var i = 0; i < playlist.length; i++) {
-        var audio = song.buildAudio(
+        var audio = this.buildAudio(
             playlist[i].title,
             playlist[i].src,
             playlist[i].img);
-        song.playlist.push(audio);
+        this.playlist.push(audio);
     }
-    return song.playlist;
+    return this.playlist;
 };
-    
+
 Song.prototype.buildAudio = function(title, src, img) {
     var aud = document.createElement('audio');
     
@@ -24,42 +25,24 @@ Song.prototype.buildAudio = function(title, src, img) {
     if (src) aud.src = src;
     if (img) aud.setAttribute('data-img', img);
     
-    aud.addEventListener('ended', song.updateHistory);
+    aud.addEventListener('ended', this.updateHistory);
     
     return aud;
 };
-    
+
 Song.prototype.updateHistory= function(e) {
-    var last = song.history.length - 1;
-    if (song.history[last] === e.target) {
+    var last = this.history.length - 1;
+    if (this.history[last] === e.target) {
         return;
         // If this song was just played, don't add it to history.
     } else {
-        song.history.push(e.target);
+        this.history.push(e.target);
     }
 };
-    
+
 Song.prototype.shuffle = function() {
     // returns playlist after sort
-    return song.playlist.sort(function() {
+    return this.playlist.sort(function() {
         return Math.floor(Math.random() * 3) - 1;
     });
 };
-
-var sampleData = [{
-    src: '//datashat.net/music_for_programming_1-datassette.mp3',
-    img: 'art/song1.png',
-    title: 'Song #1'
-}, {
-    src: '//datashat.net/music_for_programming_2-sunjammer.mp3',
-    img: 'art/song2.png',
-    title: 'Song #2'
-}, {
-    src: '//datashat.net/music_for_programming_3-datassette.mp3',
-    img: 'art/song3.png',
-    title: 'Song #3'
-}, {
-    src: '//datashat.net/music_for_programming_4-com_truise.mp3',
-    img: 'art/song4.png',
-    title: 'Song #4'
-}];
