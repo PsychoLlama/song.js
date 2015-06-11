8b7d816cb59eb28136efd8f7c2559783c92d7029
Jesse Gibson <jesse_gibson@me.com>
2015-06-11 07:17:55 +0000

    Changed song to a constructor, so you can keep multiple playlists at once.

--- song.js
+++ song.js
@@ -2,48 +2,48 @@ var song = {
     repeat: false,
     playlist: [],
     history: [],
+}
     
-    add: function(playlist) {
-        if (playlist.length === 0) return;
-        
-        for (var i = 0; i < playlist.length; i++) {
-            var audio = song.buildAudio(
-                playlist[i].title,
-                playlist[i].src,
-                playlist[i].img);
-            song.playlist.push(audio);
-        }
-        return song.playlist;
-    },
+Song.prototype.add = function(playlist) {
+    if (playlist.length === 0) return;
     
-    buildAudio: function(title, src, img) {
-        var aud = document.createElement('audio');
-        
-        if (title) aud.setAttribute('data-title', title);
-        if (src) aud.src = src;
-        if (img) aud.setAttribute('data-img', img);
-        
-        aud.addEventListener('ended', song.updateHistory);
-        
-        return aud;
-    },
-    
-    updateHistory: function(e) {
-        var last = song.history.length - 1;
-        if (song.history[last] === e.target) {
-            return;
-            // If this song was just played, don't add it to history.
-        } else {
-            song.history.push(e.target);
-        }
-    },
-    
-    shuffle: function() {
-        // returns playlist after sort
-        return song.playlist.sort(function() {
-            return Math.floor(Math.random() * 3) - 1;
-        });
+    for (var i = 0; i < playlist.length; i++) {
+        var audio = song.buildAudio(
+            playlist[i].title,
+            playlist[i].src,
+            playlist[i].img);
+        song.playlist.push(audio);
     }
+    return song.playlist;
+};
+    
+Song.prototype.buildAudio = function(title, src, img) {
+    var aud = document.createElement('audio');
+    
+    if (title) aud.setAttribute('data-title', title);
+    if (src) aud.src = src;
+    if (img) aud.setAttribute('data-img', img);
+    
+    aud.addEventListener('ended', song.updateHistory);
+    
+    return aud;
+};
+    
+Song.prototype.updateHistory= function(e) {
+    var last = song.history.length - 1;
+    if (song.history[last] === e.target) {
+        return;
+        // If this song was just played, don't add it to history.
+    } else {
+        song.history.push(e.target);
+    }
+};
+    
+Song.prototype.shuffle = function() {
+    // returns playlist after sort
+    return song.playlist.sort(function() {
+        return Math.floor(Math.random() * 3) - 1;
+    });
 };
 
 var sampleData = [{
