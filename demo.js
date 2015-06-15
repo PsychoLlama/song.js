84a12643f1873ed405f11b10d5e1de3744132264
Jesse Gibson <jesse_gibson@me.com>
2015-06-15 04:19:15 +0000

    Fixed image links in the sampleData array.

--- demo.js
+++ demo.js
@@ -3,19 +3,19 @@
 // This file loads in and injects sampleData for the demo website.
 var sampleData = [{
     src: '//datashat.net/music_for_programming_1-datassette.mp3',
-    img: 'art/song1.png',
+    img: 'album/song1.png',
     title: 'Song #1'
 }, {
     src: '//datashat.net/music_for_programming_2-sunjammer.mp3',
-    img: 'art/song2.png',
+    img: 'album/song2.png',
     title: 'Song #2'
 }, {
     src: '//datashat.net/music_for_programming_3-datassette.mp3',
-    img: 'art/song3.png',
+    img: 'album/song3.png',
     title: 'Song #3'
 }, {
     src: '//datashat.net/music_for_programming_4-com_truise.mp3',
-    img: 'art/song4.png',
+    img: 'album/song4.png',
     title: 'Song #4'
 }];
 
