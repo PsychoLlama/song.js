116d4cd678b008690b00c8e7f8a2ab180e6551b9
Jesse Gibson <jesse_gibson@me.com>
2015-06-17 05:49:29 +0000

    Scoped my file and moved some functions into local scope vs the prototype (it did not make sense there anyways).

--- song.js
+++ song.js
@@ -1,103 +1,108 @@
-"use strict";
-var Song = function(playlist) {
-    this.repeat = false;
+(function() {
+    "use strict";
     
-    this.playlist = [];
-    this.add(playlist);
     
-    this.songNumber = 0;
-};
-
-Song.prototype.history = [];
-
-Song.prototype.add = function(playlist) {
-    if (playlist.length === 0) return;
-    
-    for (var i = 0; i < playlist.length; i++) {
-        var audio = this.buildAudio(
-            playlist[i].title,
-            playlist[i].src,
-            playlist[i].img);
-        this.playlist.push(audio);
+    function buildAudio(title, src, img) {
+        var aud = document.createElement('audio');
+        
+        if (title) aud.setAttribute('data-title', title);
+        if (src) aud.src = src;
+        if (img) aud.setAttribute('data-img', img);
+        
+        return aud;
     }
-    return this.playlist;
-};
-
-Song.prototype.buildAudio = function(title, src, img) {
-    var aud = document.createElement('audio');
     
-    if (title) aud.setAttribute('data-title', title);
-    if (src) aud.src = src;
-    if (img) aud.setAttribute('data-img', img);
-    
-    return aud;
-};
-
-Song.prototype.updateHistory = function(song) {
-    var last = Song.prototype.history.length - 1;
-    if (Song.prototype.history[last] === song) {
-        return;
-        // If this song was just played, don't add it to history.
-    } else {
-        Song.prototype.history.push(song);
+    function add(playlist) {
+        if (playlist.length === 0) return;
+        
+        for (var i = 0; i < playlist.length; i++) {
+            var audio = buildAudio(
+                playlist[i].title,
+                playlist[i].src,
+                playlist[i].img);
+            this.playlist.push(audio);
+        }
+        return this.playlist;
     }
-};
-
-Song.prototype.shuffle = function() {
-    this.songNumber = 0;
     
-    // returns playlist after sort
-    return this.playlist.sort(function() {
-        return Math.floor(Math.random() * 3) - 1;
-    });
-};
-
-Song.prototype.next = function() {
-    var lastSong = this.playlist.length - 1;
-    var isLastSong = (this.songNumber === lastSong);
-    var repeat = this.repeat;
+    var Song = function(playlist) {
+        this.repeat = false;
+        
+        this.playlist = [];
+        add(playlist);
+        
+        this.songNumber = 0;
+    };
     
-    this.resetSongs();
+    Song.prototype.history = [];
     
-    if (isLastSong && repeat) {
+    
+    Song.prototype.updateHistory = function(song) {
+        var last = Song.prototype.history.length - 1;
+        if (Song.prototype.history[last] === song) {
+            return;
+            // If this song was just played, don't add it to history.
+        } else {
+            Song.prototype.history.push(song);
+        }
+    };
+    
+    Song.prototype.shuffle = function() {
         this.songNumber = 0;
         
-    } else if (isLastSong && !repeat) {
-        return undefined;
-        
-    } else if (this.songNumber < lastSong) {
-        this.songNumber++;
-    }
+        // returns playlist after sort
+        return this.playlist.sort(function() {
+            return Math.floor(Math.random() * 3) - 1;
+        });
+    };
     
-    this.updateHistory(this.getSong());
-    return this.getSong();
-};
-
-Song.prototype.previous = function() {
-    var aud = this.getSong();
-    this.resetSongs();
-    
-    if (aud.currentTime > 5) {
-        return aud;
+    Song.prototype.next = function() {
+        var lastSong = this.playlist.length - 1;
+        var isLastSong = (this.songNumber === lastSong);
+        var repeat = this.repeat;
         
-    } else if (this.songNumber === 0) {
-        this.songNumber = this.playlist.length - 1;
+        this.resetSongs();
         
-    } else if (this.songNumber > 0) {
-        this.songNumber--;
-    }
+        if (isLastSong && repeat) {
+            this.songNumber = 0;
+            
+        } else if (isLastSong && !repeat) {
+            return undefined;
+            
+        } else if (this.songNumber < lastSong) {
+            this.songNumber++;
+        }
+        
+        this.updateHistory(this.getSong());
+        return this.getSong();
+    };
     
-    this.updateHistory(this.getSong());
-    return this.getSong();
-};
-
-Song.prototype.resetSongs = function() {
-    this.playlist.forEach(function(ele) {
-        ele.pause();
-        ele.currentTime = 0;
-    });
-};
-
-Song.prototype.getSong = function() {
-    return this.playlist[this.songNumber];
-};
+    Song.prototype.previous = function() {
+        var aud = this.getSong();
+        this.resetSongs();
+        
+        if (aud.currentTime > 5) {
+            return aud;
+            
+        } else if (this.songNumber === 0) {
+            this.songNumber = this.playlist.length - 1;
+            
+        } else if (this.songNumber > 0) {
+            this.songNumber--;
+        }
+        
+        this.updateHistory(this.getSong());
+        return this.getSong();
+    };
+    
+    Song.prototype.resetSongs = function() {
+        this.playlist.forEach(function(ele) {
+            ele.pause();
+            ele.currentTime = 0;
+        });
+    };
+    
+    Song.prototype.getSong = function() {
+        return this.playlist[this.songNumber];
+    };
+});
