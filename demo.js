b3fb62ed1308847104451f3d2c112035a8dcf408
Jesse Gibson <jesse_gibson@me.com>
2015-06-20 04:55:42 +0000

    Audio files will not respond load over HTTPS, so I've hard coded in http.

--- demo.js
+++ demo.js
@@ -2,19 +2,19 @@
 
 // This file loads in and injects sampleData for the demo website.
 var sampleData = [{
-    src: '//datashat.net/music_for_programming_1-datassette.mp3',
+    src: 'http://datashat.net/music_for_programming_1-datassette.mp3',
     img: 'album/song1.png',
     title: 'Song #1'
 }, {
-    src: '//datashat.net/music_for_programming_2-sunjammer.mp3',
+    src: 'http://datashat.net/music_for_programming_2-sunjammer.mp3',
     img: 'album/song2.png',
     title: 'Song #2'
 }, {
-    src: '//datashat.net/music_for_programming_3-datassette.mp3',
+    src: 'http://datashat.net/music_for_programming_3-datassette.mp3',
     img: 'album/song3.png',
     title: 'Song #3'
 }, {
-    src: '//datashat.net/music_for_programming_4-com_truise.mp3',
+    src: 'http://datashat.net/music_for_programming_4-com_truise.mp3',
     img: 'album/song4.png',
     title: 'Song #4'
 }];
