69b46567eb2ebd724bc6df96bf68c5a2f17ce276
Jesse Gibson <jesse_gibson@me.com>
2015-06-24 05:27:05 +0000

    Sourced the images from my TechLlama domain instead of broken relative paths.

--- demo.js
+++ demo.js
@@ -3,19 +3,19 @@
 // This file loads in and injects sampleData for the demo website.
 var sampleData = [{
     src: 'http://datashat.net/music_for_programming_1-datassette.mp3',
-    img: 'album/song1.png',
+    img: '//techllama.com/dev/song/album/song1.png',
     title: 'Song #1'
 }, {
     src: 'http://datashat.net/music_for_programming_2-sunjammer.mp3',
-    img: 'album/song2.png',
+    img: '//techllama.com/dev/song/album/song2.png',
     title: 'Song #2'
 }, {
     src: 'http://datashat.net/music_for_programming_3-datassette.mp3',
-    img: 'album/song3.png',
+    img: '//techllama.com/dev/song/album/song3.png',
     title: 'Song #3'
 }, {
     src: 'http://datashat.net/music_for_programming_4-com_truise.mp3',
-    img: 'album/song4.png',
+    img: '//techllama.com/dev/song/album/song4.png',
     title: 'Song #4'
 }];
 
