358dd1fed12bafd5d2071d6830f13a2b8790ae3d
Jesse Gibson <jesse_gibson@me.com>
2015-06-24 05:30:13 +0000

    Switched the image sources back to github.

--- demo.js
+++ demo.js
@@ -3,19 +3,19 @@
 // This file loads in and injects sampleData for the demo website.
 var sampleData = [{
     src: 'http://datashat.net/music_for_programming_1-datassette.mp3',
-    img: '//techllama.com/dev/song/album/song1.png',
+    img: '//raw.githubusercontent.com/PsychoLlama/song.js/master/album/song1.png',
     title: 'Song #1'
 }, {
     src: 'http://datashat.net/music_for_programming_2-sunjammer.mp3',
-    img: '//techllama.com/dev/song/album/song2.png',
+    img: '//raw.githubusercontent.com/PsychoLlama/song.js/master/album/song2.png',
     title: 'Song #2'
 }, {
     src: 'http://datashat.net/music_for_programming_3-datassette.mp3',
-    img: '//techllama.com/dev/song/album/song3.png',
+    img: '//raw.githubusercontent.com/PsychoLlama/song.js/master/album/song3.png',
     title: 'Song #3'
 }, {
     src: 'http://datashat.net/music_for_programming_4-com_truise.mp3',
-    img: '//techllama.com/dev/song/album/song4.png',
+    img: '//raw.githubusercontent.com/PsychoLlama/song.js/master/album/song4.png',
     title: 'Song #4'
 }];
 
